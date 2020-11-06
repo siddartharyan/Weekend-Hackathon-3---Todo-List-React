fb0ba367635110f423554c12f05ebce791a9c79e
siddartharyan <68399822+siddartharyan@users.noreply.github.com>
2020-11-06 16:40:29 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,103 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
-
 function App() {
-  let [arr, setArr] = React.useState([]);
-  let [pos, setPos] = React.useState(-1);
+  const [items, setItems] = useState([]);
+  const [info, setInfo] = useState("");
+  const [editItem, setEditItem] = useState("");
+  const [index, setIndex] = useState(-1);
 
-  const update = () => {
-    let data;
-    let k = [...arr];
-    data = document.getElementById("task").value;
-    data = data.trim();
-    if (pos === -1 || data !== "") {
-      let k1 = Date.now();
-      if (data === "") {
-        return;
-      }
-      let obj = {
-        info: data,
-        uid: k1
-      };
-      k.push(obj);
-      setArr(k);
-    } else if (pos !== -1) {
-      data = document.getElementById("text" + arr[pos].uid).value;
-      data = data.trim();
-      if (data === "") {
-        return;
-      }
-      let obj = {
-        info: data,
-        uid: arr[pos].uid
-      };
-      k[pos] = obj;
-      setArr(k);
-    }
-    setPos(-1);
-    document.getElementById("task").value = "";
+  const handleChange = (event) => {
+    setInfo(event.target.value);
   };
 
-  const handleEdit = (id) => {
-    document.getElementById("t" + id).classList.remove("non");
-    let k = -1;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].uid === id) {
-        k = i;
+  const handleEditChange = (event) => {
+    setEditItem(event.target.value);
+  };
+
+  const handleEdit = (uid) => {
+    let s;
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].uid === uid) {
+        s = items[i].infor;
         break;
       }
     }
-    document.getElementById("text" + id).value = arr[k].info;
-    setPos(k);
+    setEditItem(s);
+    setIndex(uid);
   };
 
-  const handleDelete = (id) => {
-    let newarray = [];
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].uid !== id) {
-        newarray.push(arr[i]);
+  const handleDelete = (uid) => {
+    let arr = [];
+    for (let i = 0; i < items.length; i++) {
+      if (items[i].uid !== uid) {
+        arr.push(items[i]);
       }
     }
-    if (newarray.length === 0) {
-      setPos(-1);
-    }
-    setArr(newarray);
+    setItems(arr);
   };
 
-  const Get = ({ info, ke }) => {
-    let textid = "text" + ke;
+  const handleSave = () => {
+    if (editItem === "") {
+      return;
+    }
+    let obj = {
+      infor: editItem,
+      uid: index
+    };
+    let ar = [...items];
+    for (let i = 0; i < ar.length; i++) {
+      if (ar[i].uid === index) {
+        ar[i] = obj;
+      }
+    }
+    setItems(ar);
+    setEditItem("");
+    setIndex(-1);
+  };
+  const handleClick = () => {
+    if (info === "") {
+      return;
+    }
+    const obj = {
+      infor: info,
+      uid: Date.now()
+    };
+    let dupArr = [...items];
+    dupArr.push(obj);
+    setItems(dupArr);
+    setInfo("");
+  };
+
+  const Other = ({ val }) => {
     return (
-      <div className="list" key={ke}>
-        <p>{info}</p>
-        <button className="editTask" onClick={() => handleEdit(ke)}>
-          edit
+      <>
+        <input
+          onChange={handleEditChange}
+          type="string"
+          className="editTask"
+          value={val}
+        />
+        <button
+          disabled={val === ""}
+          onClick={() => handleSave()}
+          className="saveTask"
+        >
+          save
         </button>
-        <button className="delete" onClick={() => handleDelete(ke)}>
-          Delete
-        </button>
-        <div id={"t" + ke} className="non">
-          <textarea id={textid}></textarea>
-          <button className="saveTask" onClick={update}>
-            SaveTask
-          </button>
-        </div>
-      </div>
+      </>
     );
   };
 
+  const Get = ({ data }) => {
+    return (
+      <>
+        <li className="list">
+          {index === data.uid ? <Other val={editItem} /> : data.infor}
+        </li>
+        <button onClick={() => handleDelete(data.uid)}>Delete</button>
+        <button onClick={() => handleEdit(data.uid)}>edit</button>
+      </>
+    );
+  };
   return (
-    <>
-      <div id="main">
-        <textarea id="task"></textarea>
-        <button id="btn" onClick={update}>
-          Click here to add
-        </button>
-      </div>
-      {arr.map((ele) => (
-        <Get info={ele.info} ke={ele.uid} key={ele.uid} />
+    <div id="main">
+      <textarea id="task" onChange={handleChange} value={info}></textarea>
+      <button onClick={handleClick}>Add</button>
+      {items.map((ele) => (
+        <Get key={ele.uid} data={ele} editItem={editItem} />
       ))}
-    </>
+    </div>
   );
 }
-
 export default App;
